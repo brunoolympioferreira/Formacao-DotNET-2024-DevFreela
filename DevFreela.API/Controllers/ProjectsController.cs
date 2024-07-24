8360419d93620ea5b34f3a7c98d533ea2e469cac
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-07-24 17:37:25 -0300

    Padrao Options

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,11 +1,17 @@
 ﻿using DevFreela.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class ProjectsController : ControllerBase
 {
+    private readonly FreelanceTotalCostConfig _config;
+    public ProjectsController(IOptions<FreelanceTotalCostConfig> options)
+    {
+        _config = options.Value;
+    }
     [HttpGet]
     public IActionResult Get(string search)
     {
@@ -21,6 +27,10 @@ public class ProjectsController : ControllerBase
     [HttpPost]
     public IActionResult Post(CreateProjectInputModel model)
     {
+        if (model.TotalCost < _config.Minimum || model.TotalCost > _config.Maximum)
+        {
+            return BadRequest("Número fora dos limites");
+        }
         return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
     }
 
