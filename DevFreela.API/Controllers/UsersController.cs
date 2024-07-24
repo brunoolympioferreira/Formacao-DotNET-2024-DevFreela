d3a30164c15c3d6b0d7e327d710cca30242c4e48
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-07-24 17:27:50 -0300

    Criando endpoints da API

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Post()
+    {
+        return Ok();
+    }
+}
