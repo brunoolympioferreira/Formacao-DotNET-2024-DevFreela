f6de7019fcc7c4a56c9d492b620ed629528b59b9
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-07-29 09:47:21 -0300

    Implementando o acesso a dados

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,8 +1,8 @@
-﻿using DevFreela.API.Models;
+﻿using DevFreela.API.Entities;
+using DevFreela.API.Models;
 using DevFreela.API.Persistence;
-using DevFreela.API.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.API.Controllers;
 [Route("api/[controller]")]
@@ -17,52 +17,125 @@ public class ProjectsController : ControllerBase
     [HttpGet]
     public IActionResult Get(string search = "")
     {
-        return Ok();
+        var projects = _context.Projects
+            .Include(p => p.Client)
+            .Include(p => p.Freelancer)
+            .Where(p => !p.IsDeleted).ToList();
+
+        var model = projects.Select(ProjectItemViewModel.FromEntity).ToList();
+
+        return Ok(model);
     }
 
     [HttpGet("{id}")]
     public IActionResult GetById(int id)
     {
-        return Ok();
+        var project = _context.Projects
+            .Include(p => p.Client)
+            .Include(p => p.Freelancer)
+            .Include(p => p.Comments)
+            .SingleOrDefault(p => p.Id == id);
+
+        var model = ProjectViewModel.FromEntity(project);
+
+        return Ok(model);
     }
 
     [HttpPost]
     public IActionResult Post(CreateProjectInputModel model)
     {
-        if (model.TotalCost < _config.Minimum || model.TotalCost > _config.Maximum)
-        {
-            return BadRequest("Número fora dos limites");
-        }
+        var project = model.ToEntity();
+
+        _context.Projects.Add(project);
+        _context.SaveChanges();
+
         return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
     }
 
     [HttpPut("{id}")]
     public IActionResult Put(int id, UpdateProjectInputModel model)
     {
+        var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+        if (project is null)
+        {
+            return NotFound();
+        }
+
+        project.Update(model.Title, model.Description, model.TotalCost);
+
+        _context.Projects.Update(project);
+        _context.SaveChanges();
+
         return NoContent();
     }
 
     [HttpDelete("{id}")]
-    public IActionResult Delete(int id) 
+    public IActionResult Delete(int id)
     {
+        var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+        if (project is null)
+        {
+            return NotFound();
+        }
+
+        project.SetAsDeleted();
+        _context.Projects.Update(project);
+        _context.SaveChanges();
+
         return NoContent();
     }
 
     [HttpPut("{id}/start")]
-    public IActionResult Start(int id) 
+    public IActionResult Start(int id)
     {
+        var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+        if (project is null)
+        {
+            return NotFound();
+        }
+
+        project.Start();
+        _context.Projects.Update(project);
+        _context.SaveChanges();
+
         return NoContent();
     }
 
     [HttpPut("{id}/complete")]
     public IActionResult Complete(int id)
     {
+        var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+        if (project is null)
+        {
+            return NotFound();
+        }
+
+        project.Complete();
+        _context.Projects.Update(project);
+        _context.SaveChanges();
+
         return NoContent();
     }
 
     [HttpPost("{id}/comments")]
     public IActionResult PostComment(int id, CreateProjectCommentInputModel model)
     {
+        var project = _context.Projects.SingleOrDefault(p => p.Id == id);
+
+        if (project is null)
+        {
+            return NotFound();
+        }
+
+        var comment = new ProjectComment(model.Content, model.IdProject, model.IdUser);
+
+        _context.ProjectComments.Add(comment);
+        _context.SaveChanges();
+
         return Ok();
     }
 }
--- DevFreela.API/Controllers/UsersController.cs
+++ DevFreela.API/Controllers/UsersController.cs
@@ -1,20 +1,57 @@
-﻿using DevFreela.API.Models;
+﻿using DevFreela.API.Entities;
+using DevFreela.API.Models;
+using DevFreela.API.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.API.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class UsersController : ControllerBase
 {
+    private readonly DevFreelaDbContext _context;
+    public UsersController(DevFreelaDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var user = _context.Users
+            .Include(u => u.Skills)
+                .ThenInclude(u => u.Skill)
+            .SingleOrDefault(u => u.Id == id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        var model = UserViewModel.FromEntity(user);
+
+        return Ok(model);
+    }
+
     [HttpPost]
     public IActionResult Post(CreateUserInputModel model)
     {
-        return Ok();
+        var user = new User(model.FullName, model.Email, model.BirthDate);
+
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        return NoContent();
     }
 
     [HttpPost("{id}/skills")]
-    public IActionResult PostSkills(UserSkillsInputModel model)
+    public IActionResult PostSkills(int id, UserSkillsInputModel model)
     {
+        var userSkills = model.SkillIds.Select(s => new UserSkill(id, s)).ToList();
+
+        _context.UserSkills.AddRange(userSkills);
+        _context.SaveChanges();
+
         return NoContent();
     }
 
