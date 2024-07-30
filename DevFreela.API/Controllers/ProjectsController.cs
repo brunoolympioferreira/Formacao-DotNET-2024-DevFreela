26b56814a02ad04a3060d6562de8312b97f32b43
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-07-30 14:59:47 -0300

    Filtro de dados e paginacao

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -15,12 +15,14 @@ public class ProjectsController : ControllerBase
         _context = context;
     }
     [HttpGet]
-    public IActionResult Get(string search = "")
+    public IActionResult Get(string search = "", int page = 0, int size = 3)
     {
         var projects = _context.Projects
             .Include(p => p.Client)
             .Include(p => p.Freelancer)
-            .Where(p => !p.IsDeleted)
+            .Where(p => !p.IsDeleted && (search == "" || p.Title.Contains(search) || p.Description.Contains(search)))
+            .Skip(page * size)
+            .Take(size)
             .ToList();
 
         var model = projects.Select(ProjectItemViewModel.FromEntity).ToList();
