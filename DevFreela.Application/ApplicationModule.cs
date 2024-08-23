3679702855e60a6386496eeca361ad0a61e73fca
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-08-23 11:38:43 -0300

    Fluent Validation

--- DevFreela.Application/ApplicationModule.cs
+++ DevFreela.Application/ApplicationModule.cs
@@ -1,6 +1,8 @@
 ﻿using DevFreela.Application.Commands.InsertProject;
 using DevFreela.Application.Models;
 using DevFreela.Application.Services;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,7 +13,8 @@ public static class ApplicationModule
     {
         services
             .AddServices()
-            .AddHandlers();
+            .AddHandlers()
+            .AddValidation();
 
         return services;
     }
@@ -30,6 +33,15 @@ public static class ApplicationModule
             config.RegisterServicesFromAssemblyContaining<InsertProjectCommand>());
 
         services.AddTransient<IPipelineBehavior<InsertProjectCommand, ResultViewModel<int>>, ValidadeInsertProjectCommandBehavior>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddValidation(this IServiceCollection services)
+    {
+        services
+            .AddFluentValidationAutoValidation()
+            .AddValidatorsFromAssemblyContaining<InsertProjectCommand>();
 
         return services;
     }
